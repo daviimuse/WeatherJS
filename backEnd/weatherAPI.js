587f47e9022feada222936c9c68662b3e917de02
Davide <84710848+daviimuse@users.noreply.github.com>
2022-07-20 18:14:09 +0000

    Second commit

--- backEnd/weatherAPI.js
+++ backEnd/weatherAPI.js
@@ -1,7 +1,7 @@
 //API file
 var weather = require('weather-js');
 
-weather.find({search: 'Bergamo, ITA', degreeType: 'C'}, function(err, result) {
+weather.find({search: 'San Francisco, CA', degreeType: 'C'}, function(err, result) {
   if(err) console.log(err);
 
   console.log(JSON.stringify(result, null, 2));
