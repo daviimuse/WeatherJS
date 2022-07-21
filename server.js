532fb0b6df19fa7bf3372cae8645d802af672b9c
Davide <84710848+daviimuse@users.noreply.github.com>
2022-07-21 14:52:56 +0000

    Final adjustments on the server side

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ app.get('/', (req, res) => {
 	request(`https://samples.openweathermap.org/data/2.5/forecast?q=${city}&appid=a421f8d94ecd45f88adffba81708fe1f`,
 		function(error, response, body) {
 			let data = JSON.parse(body);
+			console.log(body);
 			if (response.statusCode === 200) {
-				res.send(`The weather in your city "${city}" is ${data.list[0].weather[0].description}`);
+				res.send(`The weather in your city "${city}" is ${data.list[0].weather[0].description}, the temperature is ${data.list[0].main.temp}`);
+				//${(data.list[0].main.temp-275).toFixed(2)}C
 			}
 		}
 	);
